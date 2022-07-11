29ceb0a24885cf86e88758b08706900dd81d4bf6
skles <92221023+skles@users.noreply.github.com>
2022-07-11 17:50:55 +0200

    Add latest results and update charts

--- clean_data.r
+++ clean_data.r
@@ -6,9 +6,9 @@ gen_df <- function(in_path, out_path) {
   write.csv(clean_df,out_path, row.names = FALSE)
 }
 
-gen_df('data/realWorldSchemas/28062022_combined_results.csv', 'data/realWorldSchemas/clean_data.csv')
-gen_df('data/kubernetes/28062022_180216_results.csv', 'data/kubernetes/clean_data.csv')
-gen_df('data/snowplow/28062022_181510_results.csv', 'data/snowplow/clean_data.csv')
-gen_df('data/wp/28062022_combined_results.csv', 'data/wp/clean_data.csv')
+gen_df('data/realWorldSchemas/07072022_combined_results.csv', 'data/realWorldSchemas/clean_data.csv')
+gen_df('data/kubernetes/06072022_194758_results.csv', 'data/kubernetes/clean_data.csv')
+gen_df('data/snowplow/07072022_163651_results.csv', 'data/snowplow/clean_data.csv')
+gen_df('data/wp/07072022_combined_results.csv', 'data/wp/clean_data.csv')
 
 
